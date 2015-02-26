e238669d8fe4edbea8b7b18efff8302d0e8a332b
Kevin Grandon <kevingrandon@yahoo.com>
2015-02-26 00:01:45 -0800

    Add a simple animation for pages sliding in.

--- transitions.js
+++ transitions.js
@@ -1,6 +1,18 @@
 (function(ctx) {
 
   var oldOpen = ctx.open;
+  var oldClose = ctx.close;
+
+  function animateIn(frame) {
+    // XXX: Force a sync flush to make sure we get an animation.
+    document.body.clientTop;
+    document.body.scrollIntoView();
+
+    requestAnimationFrame(() => {
+      frame.style.transform = 'translateX(0%)';
+      frame.style.opacity = '1';
+    });
+  }
 
   /**
    * When the page requests a new frame, instead slide in an iframe.
@@ -8,14 +20,21 @@
   ctx.open = function(url) {
     var nextFrame = document.createElement('iframe');
     nextFrame.src = url;
-    document.body.appendChild(nextFrame);
 
     nextFrame.style.position = 'absolute';
     nextFrame.style.top = '0px';
+    nextFrame.style.left = '0';
     nextFrame.style.height = '100%';
     nextFrame.style.width = '100%';
     nextFrame.style.border = '0';
+    nextFrame.style.opacity = '0';
     nextFrame.style.zIndex = '100000';
+    nextFrame.style.transform = 'translateX(100%)';
+    nextFrame.style.transition = 'transform 0.6s ease, opacity 0.3s linear';
+
+    document.body.appendChild(nextFrame);
+
+    animateIn(nextFrame);
   };
 
   /**
